28becf9ee31d65625b4868fb7d7f2cf17edc256d
joulsCarvajal <juliancarvajaldiaz1992@outlook.com>
2022-08-19 11:26:19 -0500

    Cambié de hello world a Hola carenalgas

---
+++ app/src/main/java/com/example/conversorunidatest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.conversorunidatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
